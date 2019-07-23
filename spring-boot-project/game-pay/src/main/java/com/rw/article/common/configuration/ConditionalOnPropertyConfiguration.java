1014c1aafdfd68a7eeeb400a29d8b0bae613999f
zhouzhongqing <1030907690@qq.com>
2019-07-23 16:41:22 +0800

    条件注解代码

--- spring-boot-project/game-pay/src/main/java/com/rw/article/common/configuration/ConditionalOnPropertyConfiguration.java
+++ spring-boot-project/game-pay/src/main/java/com/rw/article/common/configuration/ConditionalOnPropertyConfiguration.java
@@ -8,17 +8,20 @@ package com.rw.article.common.configuration;
  * @date 2019/7/18 9:49
  */
 
+import com.rw.article.pay.action.CallbackAction;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ConditionalOnProperty(prefix ="study", name="enable" , havingValue="true") // 当有了study.enable属性  ConditionalOnPropertyConfiguration 才会被创建
+//@ConditionalOnProperty(prefix ="study", name="enable" , havingValue="true") // 当有了study.enable属性  ConditionalOnPropertyConfiguration 才会被创建
+@ConditionalOnBean(CallbackAction.class)
 public class ConditionalOnPropertyConfiguration {
 
 	public ConditionalOnPropertyConfiguration(){
-		System.out.println("ConditionalOnPropertyConfiguration ");
+		System.out.println("ConditionalOnPropertyConfiguration initialize ");
 	}
 
 
